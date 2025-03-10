6dae9a7cb9f4a2c61cf4e4c4bfd302bf6d8a8fde
waleedksa130 <WALEEDKSA130@GMAIL.COM>
2025-03-10 07:01:48 +0300

    Update secondroute.dart

--- secondroute.dart
+++ secondroute.dart
@@ -1,111 +1,109 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'imageroute.dart'; // تأكد من أنك قد استوردت صفحة ImageRoute هنا.
+import 'imageroute.dart';
 
-class SecondRoute extends StatelessWidget {
-  final String name;
-  final String mobile;
+class SecondRoute extends StatefulWidget {
+  final name;
+  final mobile;
 
   const SecondRoute({super.key, required this.name, required this.mobile});
+  @override
+  State<StatefulWidget> createState() => _SecondRoute();
+}
 
+class _SecondRoute extends State<SecondRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Second Route"),
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // الكونتينر الذي يحتوي على الاسم ورقم الجوال
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.lightGreenAccent.shade100,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+                  alignment: Alignment.center,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      color: Colors.blueGrey.shade50,
+                      border: Border.all(color: Colors.blueAccent, width: 2)),
+                  child: Text(
+                    "Name: ${widget.name}" + "\n" + "Mobile: ${widget.mobile}",
+                    style: TextStyle(fontSize: 20),
                   ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    "Name: $name", // عرض الاسم
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 60, 30, 0),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                color: Colors.blueGrey.shade100,
+                border: Border.all(color: Colors.blueAccent, width: 2)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(140, 70),
+                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(8), side: BorderSide(color: Colors.blueAccent)),
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    child: Text("Google", style: TextStyle(fontSize: 25, color: Colors.white))),
+                IconButton(
+                  onPressed: () {},
+                  style: IconButton.styleFrom(
+                    fixedSize: Size(140, 70),
+                    shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(8), side: BorderSide(color: Colors.blueAccent)),
+                    backgroundColor: Colors.white,
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Mobile: $mobile", // عرض رقم الجوال
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
+                  icon: Icon(Icons.wifi_calling_3_sharp, size: 50, color: Colors.blueAccent),
+                )
+              ],
             ),
-            SizedBox(height: 30), // مسافة بين الكونتينرين
-            // الكونتينر الذي يحتوي على الأزرار
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.lightGreenAccent.shade100,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Google"),
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.wifi_calling_3_sharp, size: 50, color: Colors.teal),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        onPressed: () => Navigator.pop(context), // العودة للصفحة السابقة
-                        icon: Icon(Icons.logout, size: 50, color: Colors.redAccent),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          // الانتقال إلى صفحة ImageRoute
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ImageRoute()), // التوجيه إلى صفحة ImageRoute
-                          );
-                        },
-                        child: Text("Image"),
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                color: Colors.blueGrey.shade100,
+                border: Border.all(color: Colors.blueAccent, width: 2)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: IconButton.styleFrom(
+                      fixedSize: Size(140, 70),
+                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(8), side: BorderSide(color: Colors.blueAccent)),
+                      backgroundColor: Colors.white,
+                    ),
+                    icon: Icon(Icons.login_outlined, size: 50, color: Colors.redAccent)),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => ImageRoute()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(140, 70),
+                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(8), side: BorderSide(color: Colors.blueAccent)),
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    child: Text("Image", style: TextStyle(fontSize: 25, color: Colors.white))),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
