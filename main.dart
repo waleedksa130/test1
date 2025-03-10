3b0e4dcb3401ad1cc651056ad6501f18436f2eaa
waleedksa130 <WALEEDKSA130@GMAIL.COM>
2025-03-10 07:00:02 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'oneroute.dart';
+import 'secondroute.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
       home: OneRoute(),
